10fc5420fe11217ebe9634429313d360ce3e42ea
Martichou <martichou.andre@gmail.com>
2017-12-26 18:13:29 +0100

    kernel:sched: idle.c - fix derp

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -234,7 +234,7 @@ static void cpu_idle_loop(void)
 			check_pgt_cache();
 			rmb();
 
-			if (cpu_is_offline(cpu)) {
+			if (cpu_is_offline(cpu))
 				arch_cpu_idle_dead();
 
 			local_irq_disable();
